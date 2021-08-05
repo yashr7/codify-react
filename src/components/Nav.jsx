f308d2a56c43964c84d972eb941f6f12ba150819
Yash Raj <yashraj.pratap@gmail.com>
2021-08-05 17:35:07 +0530

    Theme Update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMusic } from "@fortawesome/free-solid-svg-icons";
+import { faMusic, faHeadphones } from "@fortawesome/free-solid-svg-icons";
 
 function Nav({ libraryStatus, setLibraryStatus }) {
   return (
     <nav>
-      <h1>Waves</h1>
+      <h1>
+        <FontAwesomeIcon icon={faHeadphones} />
+        &nbsp; CODIFY
+      </h1>
       <button onClick={() => setLibraryStatus(!libraryStatus)}>
-        Library
+        Library &nbsp;
         <FontAwesomeIcon icon={faMusic} />
       </button>
     </nav>
